af5f69556518726c887fb93c8387d91da0bd739a
Andrew Julio <andrewjulio@hotmail.com>
2019-07-27 18:57:38 +0200

    Added practice starter files

---
+++ js/math.js
@@ -0,0 +1,37 @@
+// declare program variables
+let num1;
+let num2;
+let message;
+
+// announce the program
+alert("Let's do some math!");
+
+// collect numeric input
+num1 = prompt("Please type a number");
+num1 = parseFloat(num1);
+num2 = prompt("Please type another number");
+num2 = parseFloat(num2);
+
+// check whether 2nd input is not zero & whether 1st or 2nd input are numbers
+if ( num2 === 0 ) {
+    alert("The second number is 0. You can't divide by zero. Reload and try again.");
+} 
+else if ( isNaN(num1) || isNaN(num2) ) 
+{
+    alert("At least one of the values you typed is not a number. Reload and try again.");
+} 
+else 
+{
+    // build an HTML message
+    message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
+    message += num1 + " + " + num2 + " = " + (num1 + num2);
+    message += "<br>";
+    message += num1 + " * " + num2 + " = " + (num1 * num2);
+    message += "<br>";
+    message += num1 + " / " + num2 + " = " + (num1 / num2);
+    message += "<br>";
+    message += num1 + " - " + num2 + " = " + (num1 - num2);
+  
+    // write message to web page
+    document.write(message);
+}
